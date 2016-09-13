130f7bea5852a79e6e0f07e11c47b5bfd69e6f24
gnkatchmar <gunk55@msn.com>
2016-09-13 12:02:40 -0700

    implement suggested changes

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,30 +1,30 @@
 var projectView = {};
 
-projectView.populateFilters = function() {
-  $('article').each(function() {
-    if (!$(this).hasClass('template')) {
-      var val = $(this).attr('data-category');
-      optionTag = '<option value="' + val + '">' + val + '</option>';
-      if ($('#category-filter option[value="' + val + '"]').length === 0 ) {
-        $('#category-filter').append(optionTag);
-      }
-    }
-  });
-};
-
-projectView.handleCategoryFilter = function() {
-  $('#category-filter').on('change', function() {
-    if ($(this).val()) {
-      var $selectedCategory = $('article[data-category="' + $(this).val() + '"]');
-      $('article').hide();
-      $selectedCategory.fadeIn();
-    } else {
-      $('article').fadeIn();
-      $('article.template').hide();
-    }
-    $('#category-filter').val('');
-  });
-};
+// projectView.populateFilters = function() {
+//   $('article').each(function() {
+//     if (!$(this).hasClass('template')) {
+//       var val = $(this).attr('data-category');
+//       optionTag = '<option value="' + val + '">' + val + '</option>';
+//       if ($('#category-filter option[value="' + val + '"]').length === 0 ) {
+//         $('#category-filter').append(optionTag);
+//       }
+//     }
+//   });
+// };
+//
+// projectView.handleCategoryFilter = function() {
+//   $('#category-filter').on('change', function() {
+//     if ($(this).val()) {
+//       var $selectedCategory = $('article[data-category="' + $(this).val() + '"]');
+//       $('article').hide();
+//       $selectedCategory.fadeIn();
+//     } else {
+//       $('article').fadeIn();
+//       $('article.template').hide();
+//     }
+//     $('#category-filter').val('');
+//   });
+// };
 
 projectView.handleMainNav = function() {
   $('.main-nav .tab').on('click', function(){
@@ -49,8 +49,8 @@ projectView.setTeasers = function() {
 };
 
 $(document).ready(function() {
-  projectView.populateFilters();
-  projectView.handleCategoryFilter();
+  // projectView.populateFilters();
+  // projectView.handleCategoryFilter();
   projectView.handleMainNav();
   projectView.setTeasers();
 //
