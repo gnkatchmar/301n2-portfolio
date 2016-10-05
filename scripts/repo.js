e58a57b94ac4b69af32fb7758acbf6cbc633fc76
gnkatchmar <gunk55@msn.com>
2016-10-05 12:16:51 -0700

    GitHub API, repos now on About

---
+++ scripts/repo.js
@@ -0,0 +1,33 @@
+(function(module) {
+  var repos = {};
+
+  repos.all = [];
+
+  //Retrieve my GitHub repository JSON via API
+  repos.requestRepos = function(callback) {
+    $.ajax({
+      url: 'https://api.github.com/user/repos',
+      type: 'GET',
+      headers: {
+        'Authorization': 'token ' + githubToken
+      },
+      success: function(data){
+        repos.all = data;  //added as repos.with('name').map(render) stopped working and could not fix
+        callback();
+      },
+      error: function() {
+        console.log('Error. Could not access GitHub repositories.');
+      }
+    	});
+  };
+
+  //filter of repositories based on attr - set to name (which all have by default)
+  repos.with = function(attr) {
+    console.log('Repos.With');
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
